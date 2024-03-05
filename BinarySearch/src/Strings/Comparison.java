523e2dc1af646261398c6e886037b379f0b7555e
rahulkewat001 <rahulkewat68734@gmail.com>
2024-03-05 20:33:58 +0530

    Folders added

---
+++ BinarySearch/src/Strings/Comparison.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Comparison {
+    public static void main(String[] args) {
+
+        String a = "Rahul";
+        String b = "Rahul";
+
+        // ==
+        System.out.println(a == b);
+    }
+}
